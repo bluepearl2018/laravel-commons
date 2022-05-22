25529335ae8894a5111624413accfc88cc6c1da6
mess-dev <stephane.maissin@gmail.com>
2022-05-22 12:24:40 +0100

    =Init

--- resources/views/components/create.blade.php
+++ resources/views/components/create.blade.php
@@ -8,7 +8,7 @@
 	<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
 		<a href="{{ route($routePrefix . '.'. Str::slug(Str::plural($class)) . '.index') }}"
 		   class="inline-flex items-center justify-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 text-md font-medium text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 sm:w-auto">
-			{{ __('List '. Str::title(Str::replace('-', ' ', Str::plural($class)))) }}
+			{{ __('labels.List '. Str::title(Str::replace('-', ' ', Str::plural($class)))) }}
 		</a>
 	</div>
 </div>
--- resources/views/components/show.blade.php
+++ resources/views/components/show.blade.php
@@ -13,7 +13,7 @@
 			</a>
 			<a href="{{ route($routePrefix . Str::plural(Str::slug($class)) . '.index') }}"
 			   class="inline-flex items-center justify-center rounded-md border border-transparent bg-yellow-600 px-4 py-2 text-md font-medium text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 sm:w-auto">
-				{{ __('List '. Str::title(Str::replace('_', ' ', Str::snake(Str::Studly(\Str::plural($class)))))) }}
+				{{ __('labels.List '. Str::title(Str::replace('_', ' ', Str::snake(Str::Studly(\Str::plural($class)))))) }}
 			</a>
 		</div>
 	@endauth
